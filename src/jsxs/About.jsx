cf5aa131b2421fefe3dd657737d1830116b0ffdb
Linda Lee <redtaq@hotmail.com>
2018-07-21 11:34:38 -0500

    typo check

--- src/jsxs/About.jsx
+++ src/jsxs/About.jsx
@@ -12,7 +12,7 @@ export class About extends React.Component {
           <div className="contents-wrap">
           <h1>About Me</h1>
           <p className="contents-p">Hi there! My name is Ziqing Wang. I have been studying and developing web applications for two years. To be a full-stack developer is my career goal and I am keeping working on it. I am familiar with Front-end languages as Javascript, CSS, ReactJS, JQuery, and also good at Node.js, PostgreSQL and Python. Learning and trying is the source of my enthusiasm. I love science and technology, enjoy every time applying the new tech to my projects.</p>
-          <p className="contents-p">In February 2018, I joined the coding bootcamp DigitalCrafts in Houston which greatly broadened my knowledge and enhanced my coding skills. By far, I have developed dozens of web applications including Single-Page-App, Express-React App and Python-tornado web App.</p>
+          <p className="contents-p">In February 2018, I joined the coding bootcamp DigitalCrafts in Houston which greatly broadened my knowledge and enhanced my coding skills. By far, I have developed dozens of web applications including Single-Page-App, Express-React app and Python-tornado web app.</p>
         </div>
           <Footer />
         </div>
